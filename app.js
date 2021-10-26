4173810d1c1b5f733bc5e86185d53e1f4e53dd9e
fl45hER <91982732+fl45hER@users.noreply.github.com>
2021-10-26 13:03:07 +0000

    Added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const authRouter = require("./routes/auth");
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use((err, req, res, next) => {
