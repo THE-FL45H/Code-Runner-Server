7626379532104f12744ae94f7620e2d30aeb1f20
fl45hER <91982732+fl45hER@users.noreply.github.com>
2021-10-26 09:04:01 +0000

    Setup sequelize

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use((err, req, res, next) => {
+    if (err) {
+        res.json(500).json({
+            details: "An error occurred"
+        })
+    } else {
+        next();
+    }
+})
+
 app.get("/", (req, res) => {
     res.send("Code Runner!");
 })
